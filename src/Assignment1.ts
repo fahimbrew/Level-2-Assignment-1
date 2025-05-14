ec523c7c9072cb432fc36ef9a15047f3b6bc903e
fahimbrew <iftiaqhossain@gmail.com>
2025-05-14 16:42:51 +0600

    till problem 7 has been done

--- src/Assignment1.ts
+++ src/Assignment1.ts
@@ -84,3 +84,23 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
   }
   return mostExpensive;
 }
+
+// problem 7
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  } else {
+    return "Weekday";
+  }
+}
